6e5691ef89f57266fdbf567e29a9517c651127be
zhangqiang <18353623706@163.com>
2016-05-05 13:57:15 +0800

    gradle modify

--- app/src/main/java/com/example/administrator/smallvault/ui/FragmentOne.java
+++ app/src/main/java/com/example/administrator/smallvault/ui/FragmentOne.java
@@ -1,20 +1,49 @@
 package com.example.administrator.smallvault.ui;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.administrator.smallvault.R;
+import com.github.mikephil.charting.charts.BarChart;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * 跟踪fragment
+ */
 public class FragmentOne extends Fragment {
+    @Bind(R.id.barChart)
+    BarChart barChart;
+    private View mView;
+    private MainActivity mActivity;
 
-    public FragmentOne() {
-        super();
-    }
-    
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mActivity = (MainActivity) getActivity();
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_one, container, false);
+        ButterKnife.bind(this, mView);
+        initBar();
+
+        return mView;
+    }
+
+    private void initBar() {
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
 }
